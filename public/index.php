c6f87ee6d82b0fe245f59052b1c2140414567d57
Naman Tiwari <namantiwari.nt@gmail.com>
2017-02-26 23:00:47 +0530

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<html>
+    <form action="../views/input.php" method="POST">
+	<input type="text" id="input_url"  placeholder= "Enter the url"></input><br>
+	<input type="submit"placeholder="Submit"></input>
+    </form>
+</html>
