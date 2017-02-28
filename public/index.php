91582c488c35e0bfab659db3769dcb73f2598b43
Naman Tiwari <namantiwari.nt@gmail.com>
2017-03-01 00:28:41 +0530

    Done basic framework of the app.
    Successfully taking url from user and redirecting
    him to results page further sending POST
    request through javascript for processing.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <html>
-    <form action="../views/input.php" method="POST">
-	<input type="text" id="input_url"  placeholder= "Enter the url"></input><br>
-	<input type="submit"placeholder="Submit"></input>
-    </form>
+<head>
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<form action="results.php" method="POST">
+	<input type="text" name="input_url"  placeholder= "Enter the url"></input><br>
+	<input type="submit" placeholder="Submit"></input>
+</form>
 </html>
---
+++ public/results.php
@@ -0,0 +1,34 @@
+<?php
+preg_match('/-in-([^-]+)-([^-]+)/i', $_POST["input_url"], $matches);
+$matches[1] = ucfirst($matches[1]);
+?>
+<html>
+
+<head>
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script>
+		$(document).ready(function() {
+			var url = "<?php echo $_POST["input_url"] ?>";
+        	var page = "<?php echo $matches[2]?>";
+        	var city = "<?php echo $matches[1]?>";
+        	var posting = $.post("processing.php", {input_url: url, page: page, city: city});
+        	posting.done(function(data) {
+            $("#replace").empty().append(data);
+			});
+	
+			$("#loading").fadeOut();
+			$("#request").fadeOut();
+		});
+    </script>
+</head>
+
+<body>
+	<div id="replace">
+		<h4 id="request">You requested for colleges in
+			<?php echo($matches[1]); ?> </h4>
+		<img src="img/75.gif" id="loading">
+	</div>
+</body>
+
+</html>
